a1ff6c4259894ac08096685409803d30b186f20c
raiden <jhon.jaramilloe@udea.edu.co>
2020-10-09 15:16:48 -0500

    relacion backend tablas3

---
+++ comisiones-backend-apirest/src/main/java/com/udea/comisiones/backend/apirest/models/entity/Rol.java
@@ -0,0 +1,60 @@
+package com.udea.comisiones.backend.apirest.models.entity;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="roles")
+public class Rol implements Serializable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nombre;
+	private String descripcion;
+	
+	@OneToMany(fetch = FetchType.LAZY, mappedBy = "rol")
+	private List<Usuario> usuarios;
+	
+	
+	public Rol() {
+		this.usuarios = new ArrayList<>();
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	
+	
+	public List<Usuario> getUsuarios() {
+		return usuarios;
+	}
+	public void setUsuarios(List<Usuario> usuarios) {
+		this.usuarios = usuarios;
+	}
+
+
+	private static final long serialVersionUID = 1L;
+}
