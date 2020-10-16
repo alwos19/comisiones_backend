3c757aefcb786021cf0008af47b411852cb9b8c5
AndreDaniTorres <dandrea.torres@udea.edu.co>
2020-10-16 18:35:26 -0500

    controller departamentos

--- comisiones-backend-apirest/src/main/resources/import.sql
+++ comisiones-backend-apirest/src/main/resources/import.sql
@@ -1,3 +1,15 @@
-INSERT INTO usuarios (nombre, apellido, email) VALUES ('Dana', 'Scully', '@queequeg');
-INSERT INTO usuarios (nombre, apellido, email) VALUES ('Fox', 'Mulder', '@trustno1');
-INSERT INTO usuarios (tipo_identificacion, nombre, apellido, email) VALUES ('CC', 'Alex', 'Krycek', '@');
+INSERT INTO facultades (nombre) VALUES ('FBI');
+INSERT INTO facultades (nombre) VALUES ('Sindicate');
+
+
+INSERT INTO departamentos (nombre, facultad_id) VALUES ('X-Files', 1);
+INSERT INTO departamentos (nombre, facultad_id) VALUES ('US Sindicate', 2);
+
+
+INSERT INTO usuarios (nombre, apellido, email, departamento_id) VALUES ('Dana', 'Scully', '@queequeg', 1);
+INSERT INTO usuarios (nombre, apellido, email, departamento_id) VALUES ('Fox', 'Mulder', '@trustno1', 1);
+INSERT INTO usuarios (tipo_identificacion, nombre, apellido, email, departamento_id) VALUES ('CC', 'Alex', 'Krycek', '@', 2);
+
+INSERT INTO comisiones (lugar, usuario_id) VALUES ('Tunguska', 3);
+INSERT INTO comisiones (lugar, usuario_id) VALUES ('USA', 3);
+
